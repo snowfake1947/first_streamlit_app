d01aca2fa5b217fe2bfbae1b8585faec3d1a8892
kc <114780167+snowfake1947@users.noreply.github.com>
2022-09-30 19:23:24 -0400

    first list indexed with fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ streamlit.text('🥣Raagi Pour')
 streamlit.header('🍌🥭Make your own smoothie🥝🍇')
 
 my_first_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_first_list = my_first_list.set_index('Fruit')
 streamlit.multiselect('Pick some fruits:',list(my_first_list.index))
 streamlit.dataframe(my_first_list)
