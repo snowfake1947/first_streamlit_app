5fe0b6615706de42aea6c730bd58ab779cd46733
kc <114780167+snowfake1947@users.noreply.github.com>
2022-09-30 19:04:44 -0400

    added pandas package
    called pandas to read csv file and load data frame into data frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞Bagel & Omlett')
 streamlit.text('🥗Bluberry Pancakes')
 streamlit.text('🥣Raagi Pour')
 streamlit.header('🍌🥭Make your own smoothie🥝🍇')
+
+my_first_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_first_list)
