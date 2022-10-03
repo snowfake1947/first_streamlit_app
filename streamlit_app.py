ec439569ac7bbb695e62d9b8536f2eb0df4780b3
kc <114780167+snowfake1947@users.noreply.github.com>
2022-10-03 18:43:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍞Bagel & Omlett')
